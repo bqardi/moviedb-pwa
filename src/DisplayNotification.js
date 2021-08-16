65360884c18dd4e9cf0556b0dfd8d19a51fffe72
bqardi <51706945+bqardi@users.noreply.github.com>
2021-08-16 12:19:28 +0200

    Vaibration added

--- src/DisplayNotification.js
+++ src/DisplayNotification.js
@@ -1,7 +1,9 @@
 export default function displayNotification (message) {
   if(Notification.permission === "granted") {
     navigator.serviceWorker.getRegistration().then(function (reg) {
-      reg.showNotification(message)
+      reg.showNotification(message, {
+        vibrate: [100, 50, 100, 75, 25, 25, 50, 250]
+      })
     })
   }
 }
