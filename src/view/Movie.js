5896def38b4ed20cbd895f1b6e72c501e9c9e67d
bqardi <51706945+bqardi@users.noreply.github.com>
2021-08-20 12:45:39 +0200

    Fix: missing dependency error

--- src/view/Movie.js
+++ src/view/Movie.js
@@ -46,12 +46,14 @@ function Movie({id}){
 	}, [id]);
 
 	useEffect(() => {
-		getScore(db, id, result => {
-			if (result) {
-				setScore(result.score);
-			}
-		});
-	}, [db, id]);
+		if (db.name) {
+			getScore(db, id, result => {
+				if (result) {
+					setScore(result.score);
+				}
+			});
+		}
+	}, [db, id, getScore]);
 	
 	return (
 		<article className="Movie">
@@ -71,7 +73,7 @@ function Movie({id}){
 					<h2 style={{margin: "2rem 0 0.5rem"}}>User score:</h2>
 					<div className="Movie__scores">
 						{[1, 2, 3, 4, 5].map(s => (
-							<button className="Movie__score" onClick={() => putScore(s)} onMouseEnter={() => setHoverScore(s)} onMouseLeave={() => setHoverScore(0)}>
+							<button key={s} className="Movie__score" onClick={() => putScore(s)} onMouseEnter={() => setHoverScore(s)} onMouseLeave={() => setHoverScore(0)}>
 								{(hoverScore > s - 1 || score > s - 1)
 									? <svg style={{fill: hoverScore > s - 1 ? "orange" : null}} viewBox="0 0 24 24"><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
 									: <svg viewBox="0 0 24 24"><path d="M12,15.39L8.24,17.66L9.23,13.38L5.91,10.5L10.29,10.13L12,6.09L13.71,10.13L18.09,10.5L14.77,13.38L15.76,17.66M22,9.24L14.81,8.63L12,2L9.19,8.63L2,9.24L7.45,13.97L5.82,21L12,17.27L18.18,21L16.54,13.97L22,9.24Z"></path></svg>}
